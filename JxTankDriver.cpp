398f4ea3ebba8e9393b3624ae1056a9c9ff186a5
Jeanette Müller <themaverick@themaverick.de>
2024-05-06 22:54:24 +0200

    control motors via servoshield PWM

--- JxTankDriver.cpp
+++ JxTankDriver.cpp
@@ -11,19 +11,52 @@ JxTankDriver::JxTankDriver(int16_t inputMin, int16_t inputMax, int16_t inputCent
 
 void JxTankDriver::setupLeftMotor(MODE mode, uint8_t pin1, uint8_t pin2)
 {
-    _motorLeft = new CytronMD(mode, pin1, pin2);
-    _speedLeft = 0;
-    _motorLeft->setSpeed(_speedLeft);
+    if (_isReadyLeft != true)
+    {
+        _motorLeft = new CytronMD(mode, pin1, pin2);
+        _speedLeft = 0;
+        _motorLeft->setSpeed(_speedLeft);
+        _isReadyLeft = true;
+    }
 }
+void JxTankDriver::setupLeftMotor(Adafruit_PWMServoDriver pwm, uint8_t pinDir, uint8_t pinPwm)
+{
+    if (_isReadyLeft != true)
+    {
+        _pwmLeft = pwm;
+        _pinDirLeft = pinDir;
+        _pinPwmLeft = pinPwm;
+
+        _currentMotorTypeLeft = PWMServoDriver;
+        _isReadyLeft = true;
+    }
+}
+
 void JxTankDriver::setupRightMotor(MODE mode, uint8_t pin1, uint8_t pin2)
 {
-    _motorRight = new CytronMD(mode, pin1, pin2);
-    _speedRight = 0;
-    _motorRight->setSpeed(_speedRight);
+    if (_isReadyRight != true)
+    {
+        _motorRight = new CytronMD(mode, pin1, pin2);
+        _speedRight = 0;
+        _motorRight->setSpeed(_speedRight);
+        _isReadyRight = true;
+    }
+}
+void JxTankDriver::setupRightMotor(Adafruit_PWMServoDriver pwm, uint8_t pinDir, uint8_t pinPwm)
+{
+    if (_isReadyRight != true)
+    {
+        _pwmRight = pwm;
+        _pinDirRight = pinDir;
+        _pinPwmRight = pinPwm;
+
+        _currentMotorTypeRight = PWMServoDriver;
+        _isReadyRight = true;
+    }
 }
 
 void JxTankDriver::updateMotorsWith(int16_t horizontalValue, int16_t verticalValue, uint16_t deadPoint, int16_t maxSpeed)
-{   
+{
     int16_t horizontal = constrain(horizontalValue, _inputMin, _inputMax);
     int16_t vertical = constrain(verticalValue, _inputMin, _inputMax);
 
@@ -35,7 +68,7 @@ void JxTankDriver::updateMotorsWith(int16_t horizontalValue, int16_t verticalVal
 
     if (vertical < _inputCenter - deadPoint || vertical > _inputCenter + deadPoint)
     {
-        joystickY = map(vertical, _inputMin, _inputMax, maxSpeed, -(maxSpeed));
+        joystickY = map(vertical, _inputMin, _inputMax, -maxSpeed, maxSpeed);
 
         // Convert joystick values to motor speeds
         leftMotorSpeedTarget += joystickY;
@@ -44,7 +77,7 @@ void JxTankDriver::updateMotorsWith(int16_t horizontalValue, int16_t verticalVal
 
     if (horizontal < _inputCenter - deadPoint || horizontal > _inputCenter + deadPoint)
     {
-        joystickX = map(horizontal, _inputMin, _inputMax, maxSpeed, -(maxSpeed));
+        joystickX = map(horizontal, _inputMin, _inputMax, -maxSpeed, maxSpeed);
 
         leftMotorSpeedTarget += joystickX;
         rightMotorSpeedTarget -= joystickX;
@@ -54,16 +87,85 @@ void JxTankDriver::updateMotorsWith(int16_t horizontalValue, int16_t verticalVal
     _speedLeft = constrain(leftMotorSpeedTarget, -(maxSpeed), maxSpeed);
     _speedRight = constrain(rightMotorSpeedTarget, -(maxSpeed), maxSpeed);
 
-    updateSpeed();
+    updateSpeed(deadPoint, maxSpeed);
 }
 
-void JxTankDriver::stop() {
+int16_t JxTankDriver::getCurrentSpeedLeft()
+{
+    return _speedLeft;
+}
+int16_t JxTankDriver::getCurrentSpeedRight()
+{
+    return _speedRight;
+}
+
+void JxTankDriver::stop()
+{
     _speedLeft = 0;
     _speedRight = 0;
-    updateSpeed();
+    updateSpeed(0, 10);
 }
 
-void JxTankDriver::updateSpeed() {
-    _motorLeft->setSpeed(_speedLeft);
-    _motorRight->setSpeed(_speedRight);
+void JxTankDriver::updateSpeed(uint16_t deadPoint, int16_t maxSpeed)
+{
+    updateSpeedLeft(deadPoint, maxSpeed);
+    updateSpeedRight(deadPoint, maxSpeed);
+}
+
+void JxTankDriver::updateSpeedLeft(uint16_t deadPoint, int16_t maxSpeed)
+{
+    if (_currentMotorTypeLeft == Direct)
+    {
+        _motorLeft->setSpeed(_speedLeft);
+    }
+    else if (_currentMotorTypeLeft == PWMServoDriver)
+    {
+        if (_speedLeft > 0)
+        {
+            uint16_t speed = map(_speedLeft, 0, maxSpeed, 0, 4095);
+            _pwmLeft.setPWM(_pinDirLeft, 0, 4095);
+            _pwmLeft.setPWM(_pinPwmLeft, 0, speed);
+        }
+        else if (_speedLeft < 0)
+        {
+            uint16_t speed = map(_speedLeft, -maxSpeed, 0, 4095, 0);
+            _pwmLeft.setPWM(_pinDirLeft, 0, 0);
+            _pwmLeft.setPWM(_pinPwmLeft, 0, speed);
+        }
+        else
+        {
+            _pwmLeft.setPWM(_pinDirLeft, 0, 0);
+            _pwmLeft.setPWM(_pinPwmLeft, 0, 0);
+        }
+    }
+}
+
+void JxTankDriver::updateSpeedRight(uint16_t deadPoint, int16_t maxSpeed)
+{
+    if (_currentMotorTypeRight == Direct)
+    {
+        _motorRight->setSpeed(_speedRight);
+    }
+    else if (_currentMotorTypeRight == PWMServoDriver)
+    {
+        if (_speedRight > 0)
+        {
+            uint16_t speed = map(_speedRight, 0, 255, 0, 4095 / 255 * maxSpeed);
+
+            _pwmRight.setPWM(_pinDirRight, 0, 4095);
+            _pwmRight.setPWM(_pinPwmRight, 0, speed);
+        }
+        else if (_speedRight < 0)
+        {
+            uint16_t speed = map(_speedRight, -255, 0, 4095 / 255 * maxSpeed, 0);
+
+            _pwmRight.setPWM(_pinDirRight, 0, 0);
+            _pwmRight.setPWM(_pinPwmRight, 0, speed);
+        }
+        else
+        {
+            _pwmRight.setPWM(_pinDirRight, 0, 0);
+            _pwmRight.setPWM(_pinPwmRight, 0, 0);
+        }
+    }
 }
--- JxTankDriver.h
+++ JxTankDriver.h
@@ -4,19 +4,31 @@
 
 #include "Arduino.h"
 #include <CytronMotorDriver.h>
+#include <Adafruit_PWMServoDriver.h>
 
 class JxTankDriver
 {
 public:
+
+    enum MotorType {
+        Direct, PWMServoDriver
+    };
+
     int16_t currentSpeedLeft() const { return _speedLeft; };
     int16_t currentSpeedRight() const { return _speedRight; };
 
     JxTankDriver(int16_t inputMin, int16_t inputMax, int16_t inputCenter, uint32_t analogFrequenc = 18000);
 
     void setupLeftMotor(MODE mode, uint8_t pin1, uint8_t pin2);
+    void setupLeftMotor(Adafruit_PWMServoDriver pwm, uint8_t pinDir, uint8_t pinPwm);
+
     void setupRightMotor(MODE mode, uint8_t pin1, uint8_t pin2);
+    void setupRightMotor(Adafruit_PWMServoDriver pwm, uint8_t pinDir, uint8_t pinPwm);
 
     void updateMotorsWith(int16_t horizontalValue, int16_t verticalValue, uint16_t deadPoint, int16_t maxSpeed = 255);
+    
+    int16_t getCurrentSpeedLeft();
+    int16_t getCurrentSpeedRight();
     void stop();
 
 private:
@@ -24,13 +36,25 @@ private:
     int16_t _inputMax = 500;
     int16_t _inputCenter = 0;
 
+    MotorType _currentMotorTypeLeft;
+    CytronMD *_motorLeft;
+    bool _isReadyLeft = false;
+    Adafruit_PWMServoDriver _pwmLeft;
+    uint8_t _pinDirLeft;
+    uint8_t _pinPwmLeft;
     int16_t _speedLeft = 0;
+
+    MotorType _currentMotorTypeRight;
+    CytronMD *_motorRight;
+    bool _isReadyRight = false;
+    Adafruit_PWMServoDriver _pwmRight;
+    uint8_t _pinDirRight;
+    uint8_t _pinPwmRight;
     int16_t _speedRight = 0;
 
-    CytronMD *_motorLeft;
-    CytronMD *_motorRight;
-
-    void updateSpeed();
+    void updateSpeed(uint16_t deadPoint, int16_t maxSpeed);
+    void updateSpeedLeft(uint16_t deadPoint, int16_t maxSpeed);
+    void updateSpeedRight(uint16_t deadPoint, int16_t maxSpeed);
 };
 
 #endif
